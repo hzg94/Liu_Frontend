9905b02edae2d90d5eebee6ea1b55771b67059b2
hzg20 <1960941659@qq.com>
2024-12-13 18:27:47 +0800

    feat(theme): 添加自定义主题并更新组件

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import PrimeVue from "primevue/config";
+import Noir from "./theme/Noir.ts";
 
 const app = createApp(App)
 
-app.use(PrimeVue)
+app.use(PrimeVue,{
+    theme: {
+        preset: Noir,
+        options: {
+            prefix: 'p',
+            darkModeSelector: 'system',
+            cssLayer: false,
+        }
+    }
+})
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import {PrimeVueResolver} from "@primevue/auto-import-resolver";
 import Components from 'unplugin-vue-components/vite';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,4 +14,9 @@ export default defineConfig({
             ]
         })
     ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    }
 })
